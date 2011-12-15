4cf9445a746f19dad434f17d427830548d6da23d
Matthew Tsim <mtsim88@gmail.com>
2011-12-14 23:06:17 -0500

    crappy home page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>High Technology High School</title>
+        <link type="text/css" href="template.css" rel="stylesheet" />
+	</head>
+
+	<body>
+
+    
+		<div id="header">
+        	<div id="faq_search_wrapper">
+        		FAQ | Contact
+        	</div>
+            
+            <div id="title_wrapper">
+            	<h1>HIGH TECHNOLOGY HIGH SCHOOL</h1>
+            </div>
+            
+            <div id="navbar">
+            	something
+            </div>
+        </div>
+        
+        <div id="container">
+        </div>
+        
+        <div id="footer">
+        </div>
+
+	</body>
+
+</html>
