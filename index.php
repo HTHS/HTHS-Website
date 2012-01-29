487da9727d11fbe7de42e7eaf4ec749d18e91fca
JMooneyham <JMooneyhamHTHS@gmail.com>
2012-01-29 13:33:01 -0500

    Added codeigniter and features

--- index.php
+++ index.php
@@ -1,129 +1,205 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>High Technology High School</title>
-        <link type="text/css" href="template.css" rel="stylesheet" />
-	</head>
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ *
+ */
+	define('ENVIRONMENT', 'development');
+/*
+ *---------------------------------------------------------------
+ * ERROR REPORTING
+ *---------------------------------------------------------------
+ *
+ * Different environments will require different levels of error reporting.
+ * By default development will show errors but testing and live will hide them.
+ */
 
-	<body>
+if (defined('ENVIRONMENT'))
+{
+	switch (ENVIRONMENT)
+	{
+		case 'development':
+			error_reporting(E_ALL);
+		break;
+	
+		case 'testing':
+		case 'production':
+			error_reporting(0);
+		break;
 
-    
-		<div id="header">
-			<div id="header_container">
-				<div id="header_links">
-					Search engine goes here
-				</div>
-				
-				<div id="title_wrapper">
-					<h1>HIGH TECHNOLOGY HIGH SCHOOL</h1>
-				</div>
-				
-				<div id="navbar">
-					<ul>
-						<li><a href="" class="current">Home</a></li>
-						<li><a href="">About</a></li>
-						<li><a href="">Academics</a></li>
-						<li><a href="">Clubs</a></li>
-						<li><a href="">Guidance</a></li>
-						<li><a href="">PFA</a></li>
-						<li><a href="">Downloads</a></li>
-                        <li><a href="">Contact Us</a></li>
-					</ul>
-				</div>
-			</div>
-        </div>
-        
-        <div id="container">
-        	<div id="content_left">
-				<div id="content_left_above">
-					<div id="gallery_wrapper" class="fancybox">
-						<div id="gallery">
-							<img src="makeshift_hths_image.png" />
-						</div>
-					</div>
-				</div>
-                
-				<div id="content_left_below">
-                    <div id="content_left_left">
-						<div id="feed" class="fancybox">
-							<h2 class="fancytitle">Facebook + Twitter</h2>
-							<p>insert integrated feed here</p>
-							<h2 class="fancytitle">Stay Connected</h2>
-							<table>
-                            	<tr>
-                                	<td><img src="icons/news_archive.gif" /></td>
-                                    <td><a href="">News Archive</a></td>
-                                </tr>
-                                <tr>
-                                	<td><img src="icons/rss.gif" /></td>
-                                    <td><a href="">RSS Feed</a></td>
-                                </tr>
-                                <tr>
-                                	<td><img src="icons/facebook.gif" /></td>
-                                    <td><a href="">Like Us on Facebook</a></td>
-                                </tr>
-                                <tr>
-                                	<td><img src="icons/twitter.gif" /></td>
-                                    <td><a href="">Follow Us on Twitter</a></td>
-                                </tr>
-                            </table>
-						</div>
-                    </div>	
-                    
-                    <div id="content_left_right">
-						<div id="news" class="fancybox">
-							<h2 class="fancytitle">News</h2>
-							<p> insert news here</p>
-						</div>
-                    </div>  
-				</div>
-            </div>
-            
-            <div id="sidebar">
-            	<div class="fancybox">
-                    <a href="" class="button_link">>> Prospective Students</a>             
-                    <a href="" class="button_link">>> Alumni</a>
-				</div> 
-                <div class="fancybox">
-                    <p>insert pretty google calendar here</p>
-                </div>
-                <div class="fancybox">
-                    <img src="icons/one-call-now-banner-logo.gif" /><br />
-                    <table>
-                    	<tr>
-                        	<td><img src="icons/one-call-now-banner-play-message-green-24.gif" /></td> 
-                            <td>Click here to replay the latest message from High Technology High School.</td>
-                    </tr></table>
-                </div>
-                <div class="fancybox">
-                    <img src="icons/one-call-now-banner-logo-family-profile.gif" /><br />
-                    <table>
-                    	<tr>
-                        	<td><img src="icons/plus.gif" /></td>
-                        	<td>Click here to add additional phone numbers and e-mail addresses to our automated messaging system.</td>
-                      	</tr>
-                    </table>
-                </div>
-            </div>
-            
-            <div id="clear_float">
-            </div>
-        </div>
-        
-        <div id="footer">
-			<div class="left">
-            	765 Newman Springs Road, Lincroft, New Jersey 07738<br />
-            	Phone: (732) 842-8444 | Fax: (732) 219-9418
-            </div>
-            
-            <div class="right">
-            	(c) 2012 High Technology High School <br  />
-                Non-Discrimination Clause
-            </div>
-        </div>
+		default:
+			exit('The application environment is not set correctly.');
+	}
+}
 
-	</body>
+/*
+ *---------------------------------------------------------------
+ * SYSTEM FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of your "system" folder.
+ * Include the path if the folder is not in the same  directory
+ * as this file.
+ *
+ */
+	$system_path = 'system';
 
-</html>
+/*
+ *---------------------------------------------------------------
+ * APPLICATION FOLDER NAME
+ *---------------------------------------------------------------
+ *
+ * If you want this front controller to use a different "application"
+ * folder then the default one you can set its name here. The folder
+ * can also be renamed or relocated anywhere on your server.  If
+ * you do, use a full server path. For more info please see the user guide:
+ * http://codeigniter.com/user_guide/general/managing_apps.html
+ *
+ * NO TRAILING SLASH!
+ *
+ */
+	$application_folder = 'application';
+
+/*
+ * --------------------------------------------------------------------
+ * DEFAULT CONTROLLER
+ * --------------------------------------------------------------------
+ *
+ * Normally you will set your default controller in the routes.php file.
+ * You can, however, force a custom routing by hard-coding a
+ * specific controller class/function here.  For most applications, you
+ * WILL NOT set your routing here, but it's an option for those
+ * special instances where you might want to override the standard
+ * routing in a specific front controller that shares a common CI installation.
+ *
+ * IMPORTANT:  If you set the routing here, NO OTHER controller will be
+ * callable. In essence, this preference limits your application to ONE
+ * specific controller.  Leave the function name blank if you need
+ * to call functions dynamically via the URI.
+ *
+ * Un-comment the $routing array below to use this feature
+ *
+ */
+	// The directory name, relative to the "controllers" folder.  Leave blank
+	// if your controller is not in a sub-folder within the "controllers" folder
+	// $routing['directory'] = '';
+
+	// The controller class file name.  Example:  Mycontroller
+	// $routing['controller'] = '';
+
+	// The controller function you wish to be called.
+	// $routing['function']	= '';
+
+
+/*
+ * -------------------------------------------------------------------
+ *  CUSTOM CONFIG VALUES
+ * -------------------------------------------------------------------
+ *
+ * The $assign_to_config array below will be passed dynamically to the
+ * config class when initialized. This allows you to set custom config
+ * items or override any default config values found in the config.php file.
+ * This can be handy as it permits you to share one application between
+ * multiple front controller files, with each file containing different
+ * config values.
+ *
+ * Un-comment the $assign_to_config array below to use this feature
+ *
+ */
+	// $assign_to_config['name_of_config_item'] = 'value of config item';
+
+
+
+// --------------------------------------------------------------------
+// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+// --------------------------------------------------------------------
+
+/*
+ * ---------------------------------------------------------------
+ *  Resolve the system path for increased reliability
+ * ---------------------------------------------------------------
+ */
+
+	// Set the current directory correctly for CLI requests
+	if (defined('STDIN'))
+	{
+		chdir(dirname(__FILE__));
+	}
+
+	if (realpath($system_path) !== FALSE)
+	{
+		$system_path = realpath($system_path).'/';
+	}
+
+	// ensure there's a trailing slash
+	$system_path = rtrim($system_path, '/').'/';
+
+	// Is the system path correct?
+	if ( ! is_dir($system_path))
+	{
+		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+	}
+
+/*
+ * -------------------------------------------------------------------
+ *  Now that we know the path, set the main path constants
+ * -------------------------------------------------------------------
+ */
+	// The name of THIS file
+	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+	// The PHP file extension
+	// this global constant is deprecated.
+	define('EXT', '.php');
+
+	// Path to the system folder
+	define('BASEPATH', str_replace("\\", "/", $system_path));
+
+	// Path to the front controller (this file)
+	define('FCPATH', str_replace(SELF, '', __FILE__));
+
+	// Name of the "system folder"
+	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+
+	// The path to the "application" folder
+	if (is_dir($application_folder))
+	{
+		define('APPPATH', $application_folder.'/');
+	}
+	else
+	{
+		if ( ! is_dir(BASEPATH.$application_folder.'/'))
+		{
+			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+		}
+
+		define('APPPATH', BASEPATH.$application_folder.'/');
+	}
+
+/*
+ * --------------------------------------------------------------------
+ * LOAD THE BOOTSTRAP FILE
+ * --------------------------------------------------------------------
+ *
+ * And away we go...
+ *
+ */
+require_once BASEPATH.'core/CodeIgniter.php';
+
+/* End of file index.php */
+/* Location: ./index.php */
