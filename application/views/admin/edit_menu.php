2fa0a749ff57b636a8c3a50b9df074157e03b26e
Zachary Liu <liu.zachary@gmail.com>
2012-02-26 14:02:42 -0500

    Added most of the javascript code for the menu editor.

--- application/views/admin/edit_menu.php
+++ application/views/admin/edit_menu.php
@@ -1,6 +1,55 @@
+<script type="text/javascript" src="<?=site_url()?>js/jquery.json-2.3.min.js"></script>
 <script type="text/javascript">
+var menueditor = {
+	remove_item: function(item) {
+		if (confirm('Are you sure you want to remove this item?')) {
+			$(item).remove();
+		}
+	},
+	add_item: function() {
+		var index = $('#menueditor_primitives > .menueditor_item').clone().insertBefore('#menueditor_additem').index('*');
+		return $('*').eq(index);
+	},
+	remove_link: function(link) {
+		if (confirm('Are you sure you want to remove this link?')) {
+			$(link).remove();
+		}
+	},
+	add_link: function(item) {
+		var element = $(item).find('.menueditor_item_addlink');
+		var index = $('#menueditor_primitives > .menueditor_item_link').clone().insertBefore(element).index('*');
+		return $('*').eq(index);
+	},
+	load_data: function(data) {
+		var this_class = this;
+		for item in data {
+			var item_element = this_class.add_item();
+			for link in item {
+				var link_element = this_class.add_link(item_element);
+				$(link_element).find('menueditor_item_link_title').value(link.title);
+				$(link_element).find('menueditor_item_link_url').value(link.url);
+			}
+		}
+	},
+	save_data: function() {
+		var this_class = this;
+		var data = new Array();
+		$('.menueditor_item').each(function() {
+			var data_item = new Array();
+			$(this).find('.menueditor_item_link').each(function() {
+				var data_link = new Object();
+				data_link.title = $(this).find('menueditor_item_link_title').value();
+				data_link.url = $(this).find('menueditor_item_link_url').value();
+				data_item.push(data_link);
+			});
+			data.push(data_item);
+		});
+		return $.toJSON(data);
+	}
+}
+
 $(function() {
-	var menu = <?=$menu?>;
+	//var menu = <?=$menu?>;
 });
 </script>
 
@@ -10,7 +59,7 @@ $(function() {
 	border: 1px solid #555;
 	border-bottom: 0;
 }
-.menueditor_item, .menueditor_additem {
+.menueditor_item, #menueditor_additem {
 	border-bottom: 1px solid #555;
 	
 	background: #ededed; /* Old browsers */
@@ -61,14 +110,14 @@ $(function() {
 .menueditor_item_addlink {
 	padding: 7px;
 }
-.menueditor_item_addlink .menueditor_button, .menueditor_additem .menueditor_button {
+.menueditor_item_addlink .menueditor_button, #menueditor_additem .menueditor_button {
 	margin-right: 10px;
 }
 .menueditor_item_link_remove {
 	display: inline-block;
 	margin-right: 10px;
 }
-.menueditor_additem {
+#menueditor_additem {
 	padding: 7px;
 }
 #menueditor_submit {
@@ -82,31 +131,29 @@ $(function() {
 	height: auto;
 	padding: 3px;
 }
+#menueditor_primitives {
+	display: none;
+}
 </style>
 
 <div class="fancybox">
 	<h2 class="fancytitle">Menu Editor</h2>
-	<div id="menueditor">
+	<div id="menueditor_primitives">
 		<div class="menueditor_item">
-			<div class="menueditor_item_remove"><a class="menueditor_button" href="#">-</a></div>
-			<div class="menueditor_item_linkwrapper">
-				<div class="menueditor_item_link">
-					<input class="menueditor_item_link_title"></input>&nbsp;-&gt;&nbsp;<input class="menueditor_item_link_url"></input>
-				</div>
-				<div class="menueditor_item_link">
-					<div class="menueditor_item_link_remove"><a class="menueditor_button" href="#">-</a></div>
-					<input class="menueditor_item_link_title"></input>&nbsp;-&gt;&nbsp;<input class="menueditor_item_link_url"></input>
-				</div>
-				<div class="menueditor_item_link">
-					<div class="menueditor_item_link_remove"><a class="menueditor_button" href="#">-</a></div>
-					<input class="menueditor_item_link_title"></input>&nbsp;-&gt;&nbsp;<input class="menueditor_item_link_url"></input>
-				</div>
-				<div class="menueditor_item_addlink">
-					<a class="menueditor_button" href="#">+</a> Add Submenu Item
-				</div>
-			</div>
+		    <div class="menueditor_item_remove"><a class="menueditor_button" href="#">-</a></div>
+		    <div class="menueditor_item_linkwrapper">
+		        <div class="menueditor_item_addlink">
+		            <a class="menueditor_button" href="#">+</a> Add Submenu Item
+		        </div>
+		    </div>
 		</div>
-		<div class="menueditor_additem">
+		<div class="menueditor_item_link">
+			<div class="menueditor_item_link_remove"><a class="menueditor_button" href="#">-</a></div>
+			<input class="menueditor_item_link_title"></input>&nbsp;-&gt;&nbsp;<input class="menueditor_item_link_url"></input>
+		</div>
+	</div>
+	<div id="menueditor">
+		<div id="menueditor_additem">
 			<a class="menueditor_button" href="#">+</a> Add Menu Item
 		</div>
 	</div>
