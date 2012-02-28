b69c3133eff423611800fc68b1e777bd4501419c
Zachary Liu <liu.zachary@gmail.com>
2012-02-27 21:32:19 -0500

    Redesigned the teachers/faculty directory page.

--- application/views/teachers/index.php
+++ application/views/teachers/index.php
@@ -1,88 +1,101 @@
+<style type="text/css">
+#teacherbox_container {
+	margin-right: 20px;
+}
+.teacherbox_outer {
+	width: 50%;
+}
+.teacherbox_outer:nth-child(even) {
+	margin-right: -20px;
+}
+.teacherbox {
+	margin: 10px;
+	padding: 10px;
+	box-shadow: 0px 2px 15px #AAA;
+	border: 0;
+	border-bottom: 7px solid #A70000;
+}
+.teacherbox_info {
+	height: 101px;
+}
+.teacherbox_photo {
+	float: left;
+	width: 80px;
+	height: 100%;
+	overflow: hidden;
+}
+.teacherbox_links {
+	margin-left: 90px;
+	height: 100%;
+}
+.teacherbox_link {
+	display: block;
+	width: 100%;
+	height: 25px;
+	margin-bottom: 10px;
+	border: 1px solid #CCC;
+	background-color: #777; 
+	color: #FFF !important;
+	font-size: 15px;
+	text-transform: uppercase;
+	font-weight: bold;
+	line-height: 25px;
+	text-indent: 7px;
+	text-shadow: -1px -1px #000;
+	box-shadow: 2px 2px 3px #AAA;
+}
+.teacherbox_link:active {
+	box-shadow: 1px 1px 3px #AAA;
+}
+.teacherbox_link_left {
+	float: left;
+	width: 47%;
+}
+.teacherbox_link_right {
+	margin-left: 53%;
+	width: 47%;
+	clear: right;
+}
+.teacherbox_title {
+	clear: both;
+	margin-top: 10px;
+}
+.teacherbox_name {
+	font-size: 16pt;
+	font-weight: bold;
+}
+.teacherbox_subject {
+	text-transform: uppercase;
+	font-size: 10pt;
+}
+</style>
+
 <div id="content_left">
 	<div id="content_left_above">
 		<div class="fancybox">
-			<h2 class="fancytitle black">HTHS Teachers and Faculty</h2>
-			<table border="1" width="100%" class="centerTable">
-				<tr class="header">
-					<td width="30%">Name</td>
-					<td width="20%">Subject</td>
-					<td width="10%">Voicemail Extension</td>
-					<td width="15%">Send Email</td>
-					<td width="15%">Webpage</td>
-					<td width="10%">Blog</td>
-				</tr>
+			<h2 class="fancytitle black">Teachers & Faculty</h2>
+			
+			<div id="teacherbox_container">
 				<? foreach($teachers->result() as $teacher): ?>
-					<tr>
-						<td><?=$teacher->name?></td>
-						<td><?=$teacher->subject?></td>
-						<td><?=$teacher->voicemail?></td>
-						<td><? if($teacher->email != ''): ?><a href="" id="email<?=$teacher->id?>">Send Email</a><? else: ?>Not Available<? endif; ?></td>
-						<td><? if($teacher->page_contents != ''): ?><a href="<?=site_url('home/teacher_pages/'.$teacher->id)?>">Webpage</a><? else: ?>Not Available<? endif; ?></td>
-						<td><? if($teacher->blog_count != 0): ?><a href="<?=site_url('home/blogs/'.$teacher->id)?>">Blog</a><? else: ?>Not Available<? endif; ?></td>
-					</tr>
+				<div class="teacherbox_outer">
+					<div class="teacherbox">
+						<div class="teacherbox_info">
+							<div class="teacherbox_photo"></div>
+							<div class="teacherbox_links">
+								<a class="teacherbox_link" href="<?=site_url('teachers/' . $teacher->username . '/')?>">About</a>
+								<a class="teacherbox_link teacherbox_link_left" href="<?=site_url('teachers/' . $teacher->username . '/blog')?>">Blog</a>
+								<a class="teacherbox_link teacherbox_link_right" href="<?=site_url('teachers/' . $teacher->username . '/pages')?>">Pages</a>
+								<a class="teacherbox_link" href="<?=site_url('teachers/' . $teacher->username . '/contact')?>">Contact</a>
+							</div>
+						</div>
+						<div class="teacherbox_title">
+							<div class="teacherbox_name"><?=$teacher->name?></div>
+							<div class="teacherbox_subject"><?=$teacher->subject?></div>
+						</div>
+					</div>
+				</div>
 				<? endforeach; ?>
-			</table>
+			</div>
 		</div>
 	</div>
 </div>
-
-<? foreach($teachers->result() as $teacher): 
-	if($teacher->email != ''): ?>
-		<div id="teacherEmail<?=$teacher->id?>" style="display:none;">
-			<div id="formOutput<?=$teacher->id?>"></div>
-			<table>
-				<tr>
-					<td>Your Email:</td>
-					<td><input type="text" name="email_address" id="email_address<?=$teacher->id?>" size="50" /></td>
-				</tr>
-				<tr>
-					<td>Subject:</td>
-					<td><input type="text" name="subject" id="subject<?=$teacher->id?>" size="50" /></td>
-				</tr>
-				<tr>
-					<td>Message:</td>
-					<td><textarea name="contents" id="contents<?=$teacher->id?>" rows="6" cols="53"></textarea>
-				</tr>
-				<tr>
-					<td rowspan="2">Security Code:</td>
-					<td><?=$cap['image']?></td>
-				</tr>
-				<tr>
-					<td><input type="text" name="verify" id="verify<?=$teacher->id?>" size="10" /></td>
-				<tr>
-					<td colspan="2"><input type="button" id="sendEmail<?=$teacher->id?>" value="Send Email" /></td>
-				</tr>
-			</table>
-			<br />
-			<p>All emails sent are logged, please use this for serious contact only.</p>
-		</div>
-<?  endif; 
-endforeach; ?>
-
-<script type="text/javascript">
-<? foreach($teachers->result() as $teacher): 
-	if($teacher->email != ''): ?>
-		$('#teacherEmail'+<?=$teacher->id?>).dialog({autoOpen: false, title: 'Email <?=$teacher->name?>', width: 600, height: 600});
-		$('#teacherEmail'+<?=$teacher->id?>).attr('style', '');
-		$('#email'+<?=$teacher->id?>).click(function(event) {
-			event.preventDefault();
-			$('#teacherEmail'+<?=$teacher->id?>).dialog('open');
-		});
-		$('#sendEmail'+<?=$teacher->id?>).click({ targetTeacherId: <?=$teacher->id?> }, function(event) {
-			var id = event.data.targetTeacherId;
-			$('#formOutput'+id).html('<img src="<?=site_url('images/icons/ajax-loader.gif')?>" alt="Loading" style="text-align:center;" />');
-			$.post('<?=site_url('json/send_email')?>', { teacher_id: id, email_address: $('#email_address'+id).val(), subject: $('#subject'+id).val(), message: $('#contents'+id).val(), verify: $('#verify'+id).val() }, function(response) {
-				if(response == 'TRUE') {
-					$('#formOutput'+id).html('<div class="success">Email sent successfully!</div>');
-					$('#email_address'+id).val('');
-					$('#subject'+id).val('');
-					$('#contents'+id).val('');
-					$('#verify'+id).val('');
-				}
-				else 
-					$('#formOutput'+id).html(response);
-			});
-		})
-<?  endif; 
-endforeach; ?>
-</script>
