c0695575cdbd32c32f2c4edfabbe726f962ac63e
JMooneyham <JMooneyhamHTHS@gmail.com>
2012-04-15 19:51:30 -0400

    Added the SERVER_ROOT_PATH constant in case it is needed in future.

--- application/config/constants.php
+++ application/config/constants.php
@@ -17,6 +17,7 @@ define('FILE_READ_MODE', 0644);
 define('FILE_WRITE_MODE', 0666);
 define('DIR_READ_MODE', 0755);
 define('DIR_WRITE_MODE', 0777);
+define('SERVER_ROOT_PATH', '/var/www/hths/');
 
 /*
 |--------------------------------------------------------------------------
