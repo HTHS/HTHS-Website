fba71cb17af36d7a171939b2ac392fa990ad1924
Zachary Liu <liu.zachary@gmail.com>
2012-04-15 19:57:30 -0400

    Disable menu items which link to "#", as these don't have an associated page.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(function() {
+    // Highlight currently active parent menu item when hovering over its dropdown menu. 
 	$('#navbar li ul').hover(function() {
 	    $(this).parent().find('a').addClass('current');
 	}, function() {
 	    $(this).parent().find('a').removeClass('current');
+	});
+	
+	// Disable menu items which link to "#", as these don't have an associated page. 
+	$('#navbar > ul > li > a[href$="#"]').css({'cursor': 'default'}).click(function(){
+	    return false;
 	});
 });
