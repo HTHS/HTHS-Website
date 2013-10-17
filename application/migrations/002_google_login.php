ecf54da97a40a0acb453c3f0355b25252dead405
Zachary Liu <liu.zachary@gmail.com>
2013-10-16 23:15:04 -0400

    [fix] google_users_privileges schema bug

--- application/migrations/002_google_login.php
+++ application/migrations/002_google_login.php
@@ -14,7 +14,7 @@ class Migration_Google_login extends CI_Migration {
 
         $query2 = 'CREATE TABLE `google_users_privileges` (
   `id` int(11) NOT NULL,
-  `privilege` text NOT NULL,
+  `privilege` varchar(16) NOT NULL,
   UNIQUE KEY `id` (`id`,`privilege`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
 
